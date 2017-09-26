e22df09d542bac28ae06a9a4623f3ba8d3013eb8
Joanne <joanneong225@gmail.com>
2017-09-27 00:06:58 +0800

    Correct date format to a date type for pet table

--- SQL Files/pet.sql
+++ SQL Files/pet.sql
@@ -12,19 +12,19 @@
 *   - (Photo) implement this later
 */
 
-INSERT INTO pet VALUES ('Charlie', 'dog', 'pitbull', 2003-12-3, 'small', 'xujun@gmail.com');
-INSERT INTO pet VALUES ('Rocko', 'dog', 'beagle', 2005-5-4, 'medium', 'fredKwok@gmail.com');
-INSERT INTO pet VALUES ('Tails', 'dog', 'collie', 2007-8-1, 'large', 'JessicaYong@gmail.com');
-INSERT INTO pet VALUES ('Pippen', 'dog', 'golden retriever', 2000-10-3, 'large', 'mindyZhou@gmail.com');
-INSERT INTO pet VALUES ('Power', 'dog', 'great dane', 2016-4-27, 'giant', 'jordanLim@gmail.com');
-INSERT INTO pet VALUES ('Dime', 'cat', 'ragdoll', 2013-7-5, 'giant', 'jeffereyW@gmail.com');
-INSERT INTO pet VALUES ('Shadow', 'cat', 'balinese', 2013-7-5, 'small', 'donaldChua@gmail.com');
-INSERT INTO pet VALUES ('Scarface', 'cat', 'bobcat', 2002-2-2, 'medium', 'xujun@gmail.com');
-INSERT INTO pet VALUES ('Bean', 'cat', 'maine coon', 2005-1-15, 'giant', 'huanhui@gmail.com');
-INSERT INTO pet VALUES ('Prada', 'rabbit', 'mini lop', 2012-7-20, 'small', 'gerardYu@gmail.com');
-INSERT INTO pet VALUES ('Balto', 'rabbit', 'flemish giant rabbit', 2011-3-9, 'giant', 'eric1990@gmail.com');
-INSERT INTO pet VALUES ('Salern', 'bird', 'cockatiels', 2010-3-7, 'medium', 'aliciaOng@gmail.com');
-INSERT INTO pet VALUES ('Leah', 'bird', 'verdin', 2011-7-3, 'small', 'jordanLim@gmail.com');
-INSERT INTO pet VALUES ('Dinky', 'hamster', 'syrian hamster', 2015-8-6, 'large', 'donaldChua@gmail.com');
-INSERT INTO pet VALUES ('Chanel', 'hamster', 'roborovski', 2016-1-9, 'small', 'donaldChua@gmail.com');
-INSERT INTO pet VALUES ('Postgresql', 'hamster', 'chinese hamster', 2013-2-1, 'medium', 'aliciaOng@gmail.com');
+INSERT INTO pet VALUES ('Charlie', 'dog', 'pitbull', '2003-12-3', 'small', 'xujun@gmail.com');
+INSERT INTO pet VALUES ('Rocko', 'dog', 'beagle', '2005-5-4', 'medium', 'fredKwok@gmail.com');
+INSERT INTO pet VALUES ('Tails', 'dog', 'collie', '2007-8-1', 'large', 'JessicaYong@gmail.com');
+INSERT INTO pet VALUES ('Pippen', 'dog', 'golden retriever', '2000-10-3', 'large', 'mindyZhou@gmail.com');
+INSERT INTO pet VALUES ('Power', 'dog', 'great dane', '2016-4-27', 'giant', 'jordanLim@gmail.com');
+INSERT INTO pet VALUES ('Dime', 'cat', 'ragdoll', '2013-7-5', 'giant', 'jeffereyW@gmail.com');
+INSERT INTO pet VALUES ('Shadow', 'cat', 'balinese', '2013-7-5', 'small', 'donaldChua@gmail.com');
+INSERT INTO pet VALUES ('Scarface', 'cat', 'bobcat', '2002-2-2', 'medium', 'xujun@gmail.com');
+INSERT INTO pet VALUES ('Bean', 'cat', 'maine coon', '2005-1-15', 'giant', 'huanhui@gmail.com');
+INSERT INTO pet VALUES ('Prada', 'rabbit', 'mini lop', '2012-7-20', 'small', 'gerardYu@gmail.com');
+INSERT INTO pet VALUES ('Balto', 'rabbit', 'flemish giant rabbit', '2011-3-9', 'giant', 'eric1990@gmail.com');
+INSERT INTO pet VALUES ('Salern', 'bird', 'cockatiels', '2010-3-7', 'medium', 'aliciaOng@gmail.com');
+INSERT INTO pet VALUES ('Leah', 'bird', 'verdin', '2011-7-3', 'small', 'jordanLim@gmail.com');
+INSERT INTO pet VALUES ('Dinky', 'hamster', 'syrian hamster', '2015-8-6', 'large', 'donaldChua@gmail.com');
+INSERT INTO pet VALUES ('Chanel', 'hamster', 'roborovski', '2016-1-9', 'small', 'donaldChua@gmail.com');
+INSERT INTO pet VALUES ('Postgresql', 'hamster', 'chinese hamster', '2013-2-1', 'medium', 'aliciaOng@gmail.com');
