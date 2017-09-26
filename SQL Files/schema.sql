3ece94b95a1284d9d78dcd4cb8a8e1f2a7f507d2
Ke Xiaowen <Xiaowen@196-62.priv23.nus.edu.sg>
2017-09-26 23:18:08 +0800

    modify table attributes

--- SQL Files/schema.sql
+++ SQL Files/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE account (
 	name VARCHAR(64) NOT NULL,
 	email VARCHAR(355) PRIMARY KEY,
 	password VARCHAR(50) NOT NULL,
-	region CHECK (region = 'North' OR region = 'South' OR region = 'East' OR region = 'West'),
+	region VARCHAR(10) CHECK (region = 'North' OR region = 'South' OR region = 'East' OR region = 'West'),
 	address VARCHAR(355),
 	postal_code CHAR(6)
 );
@@ -18,7 +18,7 @@ CREATE TABLE pet (
 	species VARCHAR(64),
 	dob DATE NOT NULL,
 	size VARCHAR(10) CHECK (size = 'small' OR size = 'medium' OR size = 'large' OR size = 'giant'),
-	owner REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
+	owner ﻿VARCHAR(355) REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY (pet_name, owner) 
 );
 
@@ -26,8 +26,8 @@ CREATE TABLE availability (
 	start_date DATE NOT NULL,
 	end_date DATE NOT NULL,
 	type_of_pet VARCHAR(32) NOT NULL,
-	CONSTRAINT validType CHECK (type = 'dog' OR type = 'cat' OR type = 'hamster' OR type = 'rabbit' OR type = 'bird'),
-	caretaker REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT validType CHECK (type_of_pet = 'dog' OR type_of_pet = 'cat' OR type_of_pet = 'hamster' OR type_of_pet = 'rabbit' OR type_of_pet = 'bird'),
+	caretaker VARCHAR(355) REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
 	min_bid integer NOT NULL,
 	acceptedBid boolean,
 	remark TEXT,
