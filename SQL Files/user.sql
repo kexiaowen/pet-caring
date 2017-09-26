d072e09a299d165a5dc7ee649917914049cb48b5
Ye Huanhui <huanhui.ye@gmail.com>
2017-09-26 22:40:10 +0800

    INSERT 20 user profiles

--- SQL Files/user.sql
+++ SQL Files/user.sql
@@ -1,3 +1,26 @@
 /**
 * Inserts data for users
 */
+
+INSERT INTO user VALUES ('Xu Jun','xujun@gmail.com','xu08nsd','West','22 College Avenue East','138595');
+INSERT INTO user VALUES ('Huan Hui','huanhui@gmail.com','hhh8isudis','West','12 Kent Ridge Drive','119243');
+INSERT INTO user VALUES ('Vera','vera@gmail.com','skdjskc','East','Blk 135 Jurong East St 13 #04-341','600135');
+INSERT INTO user VALUES ('Jian Yao','jianyao@gmail.com','kjdsfsdsd','South','50 COLLYER QUAY, #07-00','049321');
+INSERT INTO user VALUES ('Samuel','samuel@gmail.com','sfsdayfuds','East','8, Yung Ho Road','618590');
+INSERT INTO user VALUES ('Shasha Kumar','shasha@gmail.com','isudosjc','North','Phoenix Heights 16 Phoenix Road','668164');
+INSERT INTO user VALUES ('Taylor Goh','taylorGoh@gmail.com','dijfos','West','91 Defu Lane 10 #04-01 Swee Hin Building','539221');
+INSERT INTO user VALUES ('Mok Chen Yong','chenYong@gmail.com','sdj982h','North','2 Sembawang Walk','757616');
+INSERT INTO user VALUES ('Alicia Ong','aliciaOng@gmail.com','dkfhsnd','East','623 Aljunied Road, 02-07 Aljunied Ind. Complex','389835');
+INSERT INTO user VALUES ('Donald Chua','donaldChua@gmail.com','madiwdw','West','9 Tuas View Crescent, 637612, Singapore','637612');
+INSERT INTO user VALUES ('Fred Kwok','fredKwok@gmail.com','oo0qwd','West','48, Hillview Ter #04-01','669269');
+INSERT INTO user VALUES ('Jessica Yong','JessicaYong@gmail.com','djchsdkwwwwww','West','9 Pioneer Sector 1','628421');
+INSERT INTO user VALUES ('Gerard Yu','gerardYu@gmail.com','pppxjdyas','South','1 Pickering Street #07-02','048659');
+INSERT INTO user VALUES ('Anabelle','anabelle@gmail.com','mmmsidu','North','10 Admiralty Street Northlink Building','757695');
+INSERT INTO user VALUES ('Jefferey Woo','jeffereyW@gmail.com','qqqkchsd','South','7500A, Beach Rd #16-307, The Plaza Singapore','199591');
+INSERT INTO user VALUES ('Mindy Zhou','mindyZhou@gmail.com','kkkasfuas','East','1 Bedok South Road','469267');
+INSERT INTO user VALUES ('Elizabeth Sng','eliSng@gmail.com','zzzzzkshdfns','South','1 Kaki Bukit View #05-11 Techview','415941');
+INSERT INTO user VALUES ('Eric','eric1990@gmail.com','mnxjzsdyas','West','170 Upper Bukit Timah Road, 03-02','588179');
+INSERT INTO user VALUES ('Jordan Lim','jordanLim@gmail.com','zmxjhnjsd','East','514 Chai Chee Lane #06-03','469029');
+INSERT INTO user VALUES ('Joel Chua','joelChua@gmail.com','iueiwuew','West','2 Corporation Road #03-09/11 Corporation Place','618494');
+
+
