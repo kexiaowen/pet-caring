738daefe5886c98d99a12ed8728af334bd94faa3
Ke Xiaowen <Xiaowen@196-62.priv23.nus.edu.sg>
2017-09-26 22:47:24 +0800

    Add completed schema

--- SQL Files/schema.sql
+++ SQL Files/schema.sql
@@ -1,3 +1,45 @@
 /**
 * Specifies the schema for the project
 */
+
+CREATE TABLE user (
+	name VARCHAR(64) NOT NULL,
+	email VARCHAR(355) PRIMARY KEY,
+	password VARCHAR(50) NOT NULL,
+	region CHECK (region = 'North' OR region = 'South' OR region ='East' OR region = 'West'),
+	address VARCHAR(355),
+	postal_code CHAR(6)
+);
+
+CREATE TABLE pet (
+	pet_name VARCHAR(64),
+	type VARCHAR(32) NOT NULL,
+	CONSTRAINT validType CHECK (type = 'dog' OR type = 'cat' OR type = 'hamster' OR type = 'rabbit' OR type = 'bird'),
+	species VARCHAR(64),
+	dob DATE NOT NULL,
+	size VARCHAR(10) CHECK (size = 'small' OR size = 'medium' OR size = 'large' OR size = 'giant'),
+	owner REFERENCES user(email) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (pet_name, owner) 
+);
+
+CREATE TABLE availability (
+	start_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	type_of_pet VARCHAR(32) NOT NULL,
+	CONSTRAINT validType CHECK (type = 'dog' OR type = 'cat' OR type = 'hamster' OR type = 'rabbit' OR type = 'bird'),
+	caretaker REFERENCES user(email) ON UPDATE CASCADE ON DELETE CASCADE,
+	min_bid integer NOT NULL,
+	acceptedBid boolean,
+	remark TEXT,
+	PRIMARY KEY (start_date, end_date, caretaker)
+);
+
+CREATE TABLE bid (
+	price integer NOT NULL,
+	bidder VARCHAR(355),
+	caretaker VARCHAR(355),
+	start_date DATE,
+	end_date DATE,
+	FOREIGN KEY (caretaker, start_date, end_date) REFERENCES availability(caretaker, start_date, end_date) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (bidder, caretaker, start_date, end_date)
+);
