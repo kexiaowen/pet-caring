074b365a9659b3d7d66edcf01a3a3b776f72b8f5
Joanne <joanneong225@gmail.com>
2017-10-27 21:02:00 +0800

    Update schema

--- SQL Files/schema.sql
+++ SQL Files/schema.sql
@@ -12,15 +12,15 @@ CREATE TABLE account (
 	email VARCHAR(355) PRIMARY KEY,
 	password VARCHAR(50) NOT NULL,
 	region VARCHAR(10) CHECK (region = 'North' OR region = 'South' OR region = 'East' OR region = 'West'),
-	address VARCHAR(355),
-	postal_code CHAR(6)
+	address VARCHAR(355) NOT NULL,
+	postal_code CHAR(6) NOT NULL
 );
 
 CREATE TABLE pet (
 	pet_name VARCHAR(64),
 	type VARCHAR(32) NOT NULL,
 	CONSTRAINT validType CHECK (type = 'dog' OR type = 'cat' OR type = 'hamster' OR type = 'rabbit' OR type = 'bird'),
-	species VARCHAR(64),
+	species VARCHAR(64) NOT NULL,
 	dob DATE NOT NULL,
 	size VARCHAR(10) CHECK (size = 'small' OR size = 'medium' OR size = 'large' OR size = 'giant'),
 	owner VARCHAR(355) REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -34,19 +34,22 @@ CREATE TABLE availability (
 	CONSTRAINT validType CHECK (type_of_pet = 'dog' OR type_of_pet = 'cat' OR type_of_pet = 'hamster' OR type_of_pet = 'rabbit' OR type_of_pet = 'bird'),
 	caretaker VARCHAR(355) REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
 	min_bid integer NOT NULL,
-	acceptedBid boolean,
+	accepted_bid boolean DEFAULT false,
 	remark TEXT,
 	PRIMARY KEY (start_date, end_date, caretaker)
 );
 
 CREATE TABLE bid (
 	price integer NOT NULL,
-	bidder VARCHAR(355) REFERENCES account(email) ON UPDATE CASCADE ON DELETE CASCADE,
+	accepted_bid boolean DEFAULT false,
+	bidder VARCHAR(355),
 	caretaker VARCHAR(355),
+	pet_name VARCHAR(32),
 	start_date DATE,
 	end_date DATE,
 	FOREIGN KEY (caretaker, start_date, end_date) REFERENCES availability(caretaker, start_date, end_date) ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY (bidder, caretaker, start_date, end_date)
+    FOREIGN KEY (bidder, pet_name) REFERENCES pet(owner, pet_name),
+	PRIMARY KEY (bidder, pet_name, caretaker, start_date, end_date)
 );
 
 SET datestyle = "ISO, YMD";
